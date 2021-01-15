c9f85104a7c1806fe09b0b92595d1262adbd3a52
xianxianss <75553503+xianxianss@users.noreply.github.com>
2021-01-16 00:25:31 +0800

    增加欄位和一些API

---
+++ UnityProject/Assets/Fox.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+    [Header("移動速度"), Range(0, 50f)]
+    public float speed = 20;
+    public Rigidbody2D rig;
+    public SpriteRenderer sr;
+    private float h;
+
+    private void Move()
+    {
+        h = Input.GetAxisRaw("Horizontal");
+        rig.AddForce(new Vector2(h * speed, 0));
+
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            sr.flipX = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            sr.flipX = true;
+        }
+
+    }
+   
+
+    private void Update()
+    {
+       
+        Move();
+    }
+
+
+
+
+
+
+}
